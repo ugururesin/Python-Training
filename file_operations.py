29bb54304cb9aa7c7e7ad9c967eb4b6a876ef5da
ugur <55594186+ugururesin@users.noreply.github.com>
2021-06-06 20:13:57 +0300

    add dict to module

--- file_operations.py
+++ file_operations.py
@@ -1,2 +1,13 @@
 def get_file_ext(filename):
 	return filename[filename.index(".")+1:]
+
+programming_extensions = {
+	".c":"C and C++ source code file",
+	".cgi and .pl":"Perl script file",
+	".class":"Java class file",
+	".cpp":"C++ source code file",
+	".cs":"Visual C# source code file",
+	".h":"C, C++, and Objective-C header file",
+	".java":"Java Source code file",
+	".php":"PHP script file"
+}
